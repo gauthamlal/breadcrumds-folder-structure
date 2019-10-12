57c11d04e15568203a822a230d46f5cd6a30f44b
Gautham Lal <gautham.lal@xceler.ninja>
2019-10-12 23:59:04 +0530

    Added padding to individual breadcrumb and added home icon to home breadcrumb

--- src/components/BreadCrumbsComponent.js
+++ src/components/BreadCrumbsComponent.js
@@ -5,12 +5,19 @@ const BreadCrumbsComponent = props => {
     const { breadCrumbsList } = props;
     const list = breadCrumbsList.map((crumb, index) => {
         return (<div key={crumb.id} onClick={() => props.handleBreadCrumbClick(crumb, index)}>
-            > <span className="breadCrumb">{crumb.name}</span>
+            ><span className="breadCrumb">{crumb.name}</span>
         </div>)
     })
     return (
         <div className="breadCrumbs">
-            {[<div key='home' onClick={() => props.handleBreadCrumbClick({ id: '' }, -1)}><span className="breadCrumb">Home</span></div>, list]}
+            {[
+                <div key='home' onClick={() => props.handleBreadCrumbClick({ id: '' }, -1)}>
+                    <span className="breadCrumb" style={{ fontSize: '24px' }}>
+                        <i className="fa fa-home" aria-hidden="true"></i>
+                    </span>
+                </div>,
+                list
+            ]}
         </div>
     );
 }
